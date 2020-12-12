a940e8e1991d7e0ace8c5c874caaa43e13cc9e17
Genco Levi <genco.levi@gmail.com>
2020-12-12 12:41:45 +0300

    12 - part 2 done

--- 12/index.js
+++ 12/index.js
@@ -11,40 +11,81 @@ function run(inputFile) {
     E: { x: 1, y: 0 },
     W: { x: -1, y: 0 },
   };
-  let dirsOrdered = ["E", "N", "W", "S"];
-  let pos = { x: 0, y: 0 };
-  let dir = "E";
 
-  instructions.forEach(([inst, num]) => {
-    switch (inst) {
-      case "N":
-      case "S":
-      case "E":
-      case "W":
-        pos.x += num * dirs[inst].x;
-        pos.y += num * dirs[inst].y;
-        break;
-      case "F":
-        pos.x += num * dirs[dir].x;
-        pos.y += num * dirs[dir].y;
-        break;
-      case "L": {
-        let idx = dirsOrdered.findIndex((d) => d === dir);
-        let newIdx = (idx + num / 90) % 4;
-        dir = dirsOrdered[newIdx];
-        break;
-      }
-      case "R": {
-        let idx = dirsOrdered.findIndex((d) => d === dir);
-        let newIdx = (idx - num / 90 + 4) % 4;
-        dir = dirsOrdered[newIdx];
-        break;
-      }
-    }
-    console.log(dir, pos, inst, num);
-  });
+  function part1() {
+    let pos = { x: 0, y: 0 };
+    let dir = "E";
+    let dirsOrdered = ["E", "N", "W", "S"];
 
-  return Math.abs(pos.x) + Math.abs(pos.y);
+    instructions.forEach(([inst, num]) => {
+      switch (inst) {
+        case "N":
+        case "S":
+        case "E":
+        case "W":
+          pos.x += num * dirs[inst].x;
+          pos.y += num * dirs[inst].y;
+          break;
+        case "F":
+          pos.x += num * dirs[dir].x;
+          pos.y += num * dirs[dir].y;
+          break;
+        case "L": {
+          let idx = dirsOrdered.findIndex((d) => d === dir);
+          let newIdx = (idx + num / 90) % 4;
+          dir = dirsOrdered[newIdx];
+          break;
+        }
+        case "R": {
+          let idx = dirsOrdered.findIndex((d) => d === dir);
+          let newIdx = (idx - num / 90 + 4) % 4;
+          dir = dirsOrdered[newIdx];
+          break;
+        }
+      }
+      console.log(dir, pos, inst, num);
+    });
+
+    return Math.abs(pos.x) + Math.abs(pos.y);
+  }
+  // return part1();
+
+  function part2() {
+    let pos = { x: 0, y: 0 };
+    let waypoint = { x: 10, y: 1 };
+
+    const rotate90L = ({ x, y }) => ({ x: -y, y: x });
+    const rotate90R = ({ x, y }) => ({ x: y, y: -x });
+
+    instructions.forEach(([inst, num]) => {
+      switch (inst) {
+        case "N":
+        case "S":
+        case "E":
+        case "W":
+          waypoint.x += num * dirs[inst].x;
+          waypoint.y += num * dirs[inst].y;
+          break;
+        case "F":
+          pos.x += num * waypoint.x;
+          pos.y += num * waypoint.y;
+          break;
+        case "L": {
+          for (let i = 0; i < num / 90; i++) waypoint = rotate90L(waypoint);
+          break;
+        }
+        case "R": {
+          for (let i = 0; i < num / 90; i++) waypoint = rotate90R(waypoint);
+          break;
+        }
+      }
+      console.log(waypoint, pos, inst, num);
+    });
+
+    return Math.abs(pos.x) + Math.abs(pos.y);
+  }
+
+  return part2();
 }
 
 function parseInstruction(instruction) {
